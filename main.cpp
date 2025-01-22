392dbcd9f44f70308fa04bb403b0ab4d60ea037f
Dmytro Varchuk <loltop792@gmail.com>
2025-01-22 18:03:55 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int rec_pow(int n, int m)
+{
+    if (m == 0)
+        return 1;
+    return n * rec_pow(n, m - 1);
+}
+
+int main()
+{
+    int res = rec_pow(2, 3);
+    cout << res << endl;
+    return 0;
+}
