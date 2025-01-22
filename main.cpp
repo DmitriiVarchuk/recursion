f2de7bc1ff780432b7781d26595f68184d23e14c
Dmytro Varchuk <loltop792@gmail.com>
2025-01-22 18:05:03 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+void stars_in_row(int n) {
+	if (n == 0) {
+		return;
+	}
+	cout << "*";
+	stars_in_row(n - 1);
+}
+
+int main() {
+	int* n = new int;
+	cin >> *n;
+	stars_in_row(*n);
+
+	delete n;
+	return 0;
+}
